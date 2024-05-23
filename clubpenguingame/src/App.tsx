9213c822adfeeea860101ad8f9541d82b3109d0a
Des <ajjjjok5@gmail.com>
2024-05-23 13:47:52 -0700

    My first commit

---
+++ clubpenguingame/src/App.tsx
@@ -0,0 +1,13 @@
+import { Movement } from "./movement";
+import { Obstacles } from "./obstacles";
+
+function App() {
+  return (
+    <>
+      <Movement />
+      <Obstacles />
+    </>
+  );
+}
+
+export default App;
---
+++ clubpenguingame/src/movement.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Image from "./assets/Image.png";
+
+export const Movement: React.FC = () => {
+  const [left, setLeft] = useState<number>(0);
+  const [top, setTop] = useState<number>(0);
+
+  const handleKeyDown = (ev: KeyboardEvent) => {
+    console.log(ev.code);
+
+    switch (ev.key) {
+      case "w":
+        setTop((prevTop: number) => prevTop - 15);
+        break;
+      case "s":
+        setTop((prevTop: number) => prevTop + 15);
+        break;
+      case "a":
+        setLeft((prevLeft: number) => prevLeft - 15);
+        break;
+      case "d":
+        setLeft((prevLeft: number) => prevLeft + 15);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container2">
+        <img
+          src={Image}
+          className="image"
+          style={{ position: "relative", top: `${top}px`, left: `${left}px` }}
+        />
+      </div>
+    </>
+  );
+};
