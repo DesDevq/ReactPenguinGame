7bc68c54f3576e17104ccc4c6328cbd957c5ed50
jdi <jmotadv@gmail.com>
2024-05-24 22:31:36 -0700

    [FEAT] Use of Player and Playground components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Movement } from "./movement";
-import { Obstacles } from "./obstacles";
+import { Playground } from './layouts/Playground/Playground'
+import { Player } from './components/Player/Player'
 
-function App() {
+export const App = () =>
+{
   return (
     <>
-      <Movement />
-      <Obstacles />
+      <Playground>
+        <Player id="player1" position={{ x: 0, y: 0 }} size={{ width: 150, height: 150 }} />
+      </Playground>
     </>
-  );
+  )
 }
-
-export default App;
